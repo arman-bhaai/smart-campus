b5998e07a420362cd08c93cdefce545d9783a635
Abu Bakar Siddique Arman <arman.bhaai@gmail.com>
2023-06-02 06:07:05 +0600

    Modularize landing page

--- backend_app/views.py
+++ backend_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
 
-# Create your views here.
+# views
+def home(request):
+    tmp = loader.get_template('home.html')
+    return HttpResponse(tmp.render())
